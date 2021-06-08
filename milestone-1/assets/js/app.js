a7b3e50451ae9eac0a7b925addb8b961a1f22740
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-06-08 11:12:32 +0200

    Build request to theMovieDB API using Axios

--- milestone-1/assets/js/app.js
+++ milestone-1/assets/js/app.js
@@ -1,11 +1,17 @@
 const app = new Vue({
     el: "#app",
 
-    data: {},
+    data: {
+        query: ""
+    },
 
     methods: {},
 
     mounted() {
-        
+        axios
+        .get("https://api.themoviedb.org/3/search/movie?api_key=a38c554ce7236d631b800436519a221a&query=" + this.query)
+        .then(response => {
+            console.log(response.data.results);
+        })
     }
 });
