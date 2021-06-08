5191dcc00ccb09ace4563c1ecb36589bd99ed75d
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-06-08 11:15:33 +0200

    Add search bar and button and move request over to search method

--- milestone-1/assets/js/app.js
+++ milestone-1/assets/js/app.js
@@ -2,16 +2,24 @@ const app = new Vue({
     el: "#app",
 
     data: {
-        query: ""
+        query: "",
+
+        results: {
+            
+        }
     },
 
-    methods: {},
+    methods: {
+        search() {
+            axios
+            .get("https://api.themoviedb.org/3/search/movie?api_key=a38c554ce7236d631b800436519a221a&query=" + this.query)
+            .then(response => {
+                console.log(response.data.results);
+            })
+        }
+    },
 
     mounted() {
-        axios
-        .get("https://api.themoviedb.org/3/search/movie?api_key=a38c554ce7236d631b800436519a221a&query=" + this.query)
-        .then(response => {
-            console.log(response.data.results);
-        })
+        
     }
 });
