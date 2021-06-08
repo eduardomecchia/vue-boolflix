59300700ffa4c0820428ae1be782bc007c638c7b
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-06-08 11:56:21 +0200

    Add dynamically built flag to results and handle the English particular case

--- milestone-2/assets/js/app.js
+++ milestone-2/assets/js/app.js
@@ -15,8 +15,15 @@ const app = new Vue({
             .get("https://api.themoviedb.org/3/search/movie?api_key=a38c554ce7236d631b800436519a221a&query=" + this.query)
             .then(response => {
                 this.results = (response.data.results);
-                console.log(this.results);
             })
+        },
+
+        getFlag(flagId) {
+            if (flagId === "en") {
+                flagId = "us"
+            }
+            
+            return `https://www.countryflags.io/${flagId}/shiny/32.png`;
         }
     }
 });
