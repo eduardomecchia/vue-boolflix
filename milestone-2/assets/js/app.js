260568eda892bb04dc70b0cd9dc0291e9eecc598
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-06-08 11:34:16 +0200

    Copy and paste everything over to /milestone-2

---
+++ milestone-2/assets/js/app.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+    el: "#app",
+
+    data: {
+        query: "",
+
+        results: {
+
+        }
+    },
+
+    methods: {
+        search() {
+            axios
+            .get("https://api.themoviedb.org/3/search/movie?api_key=a38c554ce7236d631b800436519a221a&query=" + this.query)
+            .then(response => {
+                this.results = (response.data.results);
+                console.log(this.results);
+            })
+        }
+    }
+});
