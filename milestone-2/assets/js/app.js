1c9ad27e6f2a524baf94b6790a49be347c3c952e
Eduardo Mecchia <eduardomecchia273@gmail.com>
2021-06-08 12:00:28 +0200

    Comment search() and getFlag() using JSDoc

--- milestone-2/assets/js/app.js
+++ milestone-2/assets/js/app.js
@@ -10,6 +10,9 @@ const app = new Vue({
     },
 
     methods: {
+        /**
+         * Fires a request to theMovieDB API and gets the movies which name includes the query
+         */
         search() {
             axios
             .get("https://api.themoviedb.org/3/search/movie?api_key=a38c554ce7236d631b800436519a221a&query=" + this.query)
@@ -18,11 +21,16 @@ const app = new Vue({
             })
         },
 
+        /**
+         * Dynamically gets a flag from the Country Flags API
+         * @param {string} flagId 
+         * @returns An URL that points to the requested flag
+         */
         getFlag(flagId) {
             if (flagId === "en") {
                 flagId = "us"
             }
-            
+
             return `https://www.countryflags.io/${flagId}/shiny/32.png`;
         }
     }
